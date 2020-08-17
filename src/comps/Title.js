ed8af29d0daebe41b1e6b723ca069543ffe2d928
Amotsie <motsie.at@gmail.com>
2020-08-17 22:22:53 +0300

    title and Icon change

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,8 +3,8 @@ import React from "react";
 const Title = () => {
   return (
     <div className="title">
-      <h1>ISTANBULgram</h1>
-      <h2>Istanbul Pictures</h2>
+      <h1>PICTUREgram</h1>
+      <h2>Turkey Pictures</h2>
       <p>Shot by Motsie Andreas</p>
     </div>
   );
